edaf274c89830c1c6940d8e4206e9e6b2d4253f1
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-06-24 16:48:49 +0700

    can display all todo items in todo list

--- gotham-todo/test/pages/index.spec.ts
+++ gotham-todo/test/pages/index.spec.ts
@@ -5,44 +5,48 @@ describe('TodoApp', () => {
   const wrapper = mount(Index)
 
   describe('on-mounted', () => {
-     it('creates component', () => {
-         expect(wrapper.isVueInstance()).toBeTruthy()
-     })
+    it('creates component', () => {
+      expect(wrapper.isVueInstance()).toBeTruthy()
+    })
 
-     it('has default todo list', () => {
-         expect(wrapper.vm.$data.todoList).toEqual([
-             'item 1',
-             'item 2',
-             'item 3'
-         ])
-     })
+    it('has default todo list', () => {
+      expect(wrapper.vm.$data.todoList).toEqual([
+        'item 1',
+        'item 2',
+        'item 3'
+      ])
+    })
 
-      it('has default newTodo as empty string', () => {
-          expect(wrapper.vm.$data.newTodo).toEqual('')
-      })
+    it('has default newTodo as empty string', () => {
+      expect(wrapper.vm.$data.newTodo).toEqual('')
+    })
   })
 
   describe('todo list', () => {
-    xit('can display all todo items in todo list', () => {
-
+    it('can display all todo items in todo list', () => {
+      wrapper.setData({
+        todoList: ['i1']
+      })
+      const wrappers = wrapper.findAll('.item-text').at(0).text()
+      expect(wrappers).toBe('i1')
     })
 
-    xit('can add new todo to the todo todo list', () => {})
+    xit('can add new todo to the todo todo list', () => { })
 
-    xit('should clear new todo input once todo is added', () => {})
+    xit('should clear new todo input once todo is added', () => { })
   })
 
   describe('todo item', () => {
-    xit('can be marked as done', () => {})
+    xit('can be marked as done', () => { })
 
-    xit('can be deleted', () => {})
+    xit('can be deleted', () => { })
   })
 
   describe('filters', () => {
-    xit('can filters only done items', () => {})
+    xit('can filters only done items', () => { })
 
-    xit('can filters only undone items', () => {})
+    xit('can filters only undone items', () => { })
 
-    xit('can show all items', () => {})
+    xit('can show all items', () => { })
   })
 })
